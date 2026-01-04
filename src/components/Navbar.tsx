44d9f35f67cdaa1eca03a41cd159ac2dd5c4f28d
Göktuğ Özfen <goktugozfen@gmail.com>
2026-01-04 13:38:48 +0300

    Add new icon files for the application
    - Introduced multiple .ico files to enhance the visual representation of the application.
    - The new icons include various sizes and formats to ensure compatibility across different platforms and resolutions.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,37 @@
 "use client";
-
-import Link from "next/link";
-import darkStar from "@/public/darkStar.svg";
 import Image from "next/image";
+import starr from "@/public/starr.ico";
+import { Dot } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="w-full bg-neutral-950 text-white px-4 py-6 flex justify-between items-center gap-4">
       {/* Ana başlık */}
 
-      <div>
-        <Link href="/">
-            <Image src={darkStar} alt="Dark Star Logo" className="size-8"  />
-        </Link>
+      <div className="ml-2">
+        <button className="hover:scale-110">
+          <Image
+            src={starr}
+            width={50}
+            height={50}
+            alt="Picture of the author"
+          />
+        </button>
       </div>
       <div className="">
-        <Link
-          href="/"
-          className="text-3xl font-bold bg-gray-800 text-current px-6 py-2 rounded-full hover:scale-105 transition"
-        >
+        <button className="text-xl font-bold bg-neutral-800  hover:bg-gray-700 hover:scale-105 text-zinc-300 border px-6 py-2 rounded-full  transition">
           RIYAPOLIS
-        </Link>
+        </button>
       </div>
 
       {/* Menü butonları */}
       <div className="flex gap-4">
-        <Link
-          href="#"
-          className=" bg-gray-800 px-4 py-2 rounded-2xl hover:bg-gray-700 transition text-sm"
-        >
-          PHOTO
-        </Link>
-        <Link
-          href="#"
-          className="bg-gray-800 px-4 py-2 rounded-2xl hover:bg-gray-700 transition text-sm"
-        >
-          ENGINE
-        </Link>
+        <button className=" bg-neutral-800 px-7 py-4   rounded-4xl hover:bg-gray-700 transition hover:scale-105 border text-xl text-zinc-300">
+          TR
+        </button>
+        <button className="hover:scale-220 transition p-0">
+          <Dot size={60} />
+        </button>
       </div>
     </nav>
   );
